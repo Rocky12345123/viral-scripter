0905aff853a95b0953088c1449c59b0bbf1474a4
Rocky12345123 <98189177+Rocky12345123@users.noreply.github.com>
2026-06-18 12:25:29 -0600

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        padding: 40,
+        fontFamily: "sans-serif",
+        color: "white",
+        background: "black",
+        height: "100vh",
+      }}
+    >
+      <h1>Viral Scripter</h1>
+      <p>Frontend is working.</p>
+    </div>
+  );
+}
